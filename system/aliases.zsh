d1c8745d3c598cf2ccac9f1ba3404d0910e0a31c
Johan Svensson <johan@atomicplayboy.net>
2012-09-23 21:40:43 +0200

    Move system aliases out of zsh's folder

---
+++ system/aliases.zsh
@@ -0,0 +1,18 @@
+# Get local IP, yoinked from @henrik
+# https://github.com/henrik/dotfiles
+alias ip='ifconfig en1 | grep inet | grep -v inet6 | cut -c 7-16'
+
+# grc overrides for colors 'n' stuff
+if $(gls &>/dev/null)
+then
+	alias ls="gls -F --color"
+	alias l="gls -lAh --color"
+	alias ll="gls -l --color"
+	alias la="gls -A --color"
+fi
+
+# OS stuff
+alias pg="ps aux | grep"
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,27 +5,3 @@ alias reload!='. ~/.zshrc'
 alias zshconfig="st ~/.zshrc"
 alias ohmyzsh="st ~/.oh-my-zsh"
 alias dots="st ~/.dotfiles"
-
-# Local IP, yoinked from @henrik
-alias ip='ifconfig en1 | grep inet | grep -v inet6 | cut -c 7-16'
-
-# grc overrides for colors 'n' stuff
-if $(gls &>/dev/null)
-then
-	alias ls="gls -F --color"
-	alias l="gls -lAh --color"
-	alias ll="gls -l --color"
-	alias la="gls -A --color"
-fi
-
-# OS stuff
-alias pg="ps aux | grep"
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-
-# brew install autojump
-# https://github.com/joelthelion/autojump
-if [ -f `brew --prefix`/etc/autojump ]; then
-  . `brew --prefix`/etc/autojump
-fi
