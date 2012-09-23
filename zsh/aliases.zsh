9dc3906e6c7a6a21c65b1fa499070896d85013ea
Johan Svensson <johan@atomicplayboy.net>
2012-09-23 12:04:39 +0200

    New system aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,13 @@
+# Reload the whole shebang
 alias reload!='. ~/.zshrc'
 
-# Assorted aliases to fire up Sublime on stuff
+# Assorted aliases to fire up Sublime
 alias zshconfig="st ~/.zshrc"
 alias ohmyzsh="st ~/.oh-my-zsh"
-alias dotfiles="st ~/.dotfiles"
+alias dots="st ~/.dotfiles"
+
+# Local IP, yoinked from @henrik
+alias ip='ifconfig en1 | grep inet | grep -v inet6 | cut -c 7-16'
 
 # grc overrides for colors 'n' stuff
 if $(gls &>/dev/null)
@@ -12,4 +16,16 @@ then
 	alias l="gls -lAh --color"
 	alias ll="gls -l --color"
 	alias la="gls -A --color"
+fi
+
+# OS stuff
+alias pg="ps aux | grep"
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+# brew install autojump
+# https://github.com/joelthelion/autojump
+if [ -f `brew --prefix`/etc/autojump ]; then
+  . `brew --prefix`/etc/autojump
 fi
