ed3e31c788854664506da9d99b772a2151132cc3
Johan Svensson <johan@atomicplayboy.net>
2012-09-23 02:24:03 +0200

    More git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,3 +12,5 @@ fi
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gs='git status -sb'
 alias gcd='cd $(git rev-parse --show-toplevel)'
+alias gissues='git browse -- issues'	# View issues on github
+alias gwiki='git browse -- wiki'			# View wiki on github
