42d1d24d603fffaf0b58dbe87dc4a2145e7dffc6
Johan Svensson <johan@atomicplayboy.net>
2012-12-16 14:46:18 +0100

    New auto jump zsh function

--- system/autojump.zsh
+++ system/autojump.zsh
@@ -1,5 +1,5 @@
 # brew install autojump
 # https://github.com/joelthelion/autojump
 if [ -f `brew --prefix`/etc/autojump ]; then
-  . `brew --prefix`/etc/autojump
+	. `brew --prefix`/etc/autojump.zsh
 fi
