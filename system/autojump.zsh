130b0b0fa06d983d840158084d01c27a4d78acf5
Johan Svensson <johan@atomicplayboy.net>
2012-12-18 18:00:41 +0100

    Autojump fix

--- system/autojump.zsh
+++ system/autojump.zsh
@@ -1,5 +1,5 @@
 # brew install autojump
 # https://github.com/joelthelion/autojump
-if [ -f `brew --prefix`/etc/autojump ]; then
+if [ -f `brew --prefix`/etc/autojump.zsh ]; then
 	. `brew --prefix`/etc/autojump.zsh
 fi
