b5fb50aac09142f6da78ec1f20a009cd55b36b2a
Johan Svensson <johan@atomicplayboy.net>
2014-12-11 10:54:46 +0100

    Use Homebrew's PHP

--- brew/aliases.zsh
+++ brew/aliases.zsh
@@ -1,1 +1,4 @@
 alias brews="brew list -l"
+
+# Use homebrew PHP
+export PATH="$(brew --prefix homebrew/php/php56)/bin:$PATH"
---
+++ brew/install-php.sh
@@ -0,0 +1,5 @@
+brew update
+brew tap homebrew/dupes
+brew tap homebrew/php
+brew install php56
+brew install composer
